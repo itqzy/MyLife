c8e6c0f2d25b743457ba058b1107589a02e69d3f
戚志远 <qizhiyuan@seanai.cn>
2023-11-24 18:37:31 +0800

    1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'itqzy', // Usually your GitHub org/user name.
-  projectName: 'MyLife', // Usually your repo name.
+  projectName: 'My Life', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
